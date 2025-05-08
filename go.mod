af92ca5ac9a0bb5ff2374a54fd61054db239a030
Danny Bernier <dannybernier5@gmail.com>
2025-05-08 13:41:35 -0400

    added endpoint for wikisteps

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.2
 require (
 	github.com/Pallinder/go-randomdata v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
--- rest_api/main.go
+++ rest_api/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
+	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 
 	"app/rest_api/logging"
 	wikiSteps "app/rest_api/wiki_steps"
+
+	"github.com/gorilla/mux"
 )
 
-var start = "https://en.wikipedia.org/wiki/Friedrich_Merz"
-var finish = "https://en.wikipedia.org/wiki/Machine_translation"
+// var start = "https://en.wikipedia.org/wiki/Friedrich_Merz"
+// var finish = "https://en.wikipedia.org/wiki/Machine_translation"
 
 var (
-	App Application
+	App             Application
+	WikiStepService *wikiSteps.WikiSteps
 )
 
 type Application struct {
@@ -26,6 +32,48 @@ func initApplication() {
 	}
 }
 
+//curl -X GET "http://localhost:8000/wikisteps?start=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFriedrich_Merz&target=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMachine_translation&steps=5"
+func invokeWikiStepService(w http.ResponseWriter, r *http.Request) {
+	var err error
+	quaryParams := r.URL.Query()
+	start := quaryParams.Get("start")
+	target := quaryParams.Get("target")
+	steps := quaryParams.Get("steps")
+
+	if start, err = url.QueryUnescape(start); err != nil {
+		http.Error(w, "One or more required query parameters is invalid", http.StatusBadRequest)
+	}
+
+	if target, err = url.QueryUnescape(target); err != nil {
+		http.Error(w, "One or more required query parameters is invalid", http.StatusBadRequest)
+	}
+
+	if start == "" || target == "" || steps == "" {
+		http.Error(w, "Missing one or more required query parameters", http.StatusBadRequest)
+	}
+
+	stepsNum, err := strconv.Atoi(steps)
+	if err != nil {
+		http.Error(w, "One or more required query parameters is invalid", http.StatusBadRequest)
+	}
+
+	paths, err := WikiStepService.FindValidPaths(start, target, stepsNum)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error occored when finding valid paths: %s", err.Error()), http.StatusInternalServerError)
+	}
+
+	response := map[string]interface{}{
+		"start":      start,
+		"target":     target,
+		"steps":      stepsNum,
+		"validPaths": paths,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
 	initApplication()
 	App.log.Info("Application initialized!")
@@ -34,18 +82,11 @@ func main() {
 	numWorkers := 25
 	stepTimeout := 30 * time.Second
 
-	serviceWikisteps := wikiSteps.NewWikistepsService(App.log, maxSteps, stepTimeout, numWorkers)
-	results, err := serviceWikisteps.FindValidPaths(start, finish, 5)
-	if err != nil {
-		App.log.Error(err.Error())
-	}
+	WikiStepService = wikiSteps.NewWikistepsService(App.log, maxSteps, stepTimeout, numWorkers)
 
-	if results == nil {
-		App.log.Info("Didnt get results")
-	} else {
-		App.log.Info(fmt.Sprintf("Found %d Valid Paths:", len(results)))
-		for _, path := range results {
-			App.log.Info(fmt.Sprintf("Steps: %d, Path: [%s]", len(path)-1, strings.Join(path, ", ")))
-		}
-	}
+	router := mux.NewRouter()
+
+	router.HandleFunc("/wikisteps", invokeWikiStepService).Methods("GET")
+
+	App.log.Fatal(http.ListenAndServe(":8000", router).Error())
 }
